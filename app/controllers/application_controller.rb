c4085ae94f4ca8638d56d13f32cfb98367ad294f
Peter Mangiafico <peter@mangiafico.org>
2017-03-08 11:52:08 -0800

    improve form layout with bootstrap; update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def post_only
+    redirect_to root_path unless request.post?
+  end
+
 end
--- app/controllers/average_controller.rb
+++ app/controllers/average_controller.rb
@@ -1,36 +1,32 @@
 class AverageController < ApplicationController
 
+  before_filter :post_only, only: :compute
+
   def index
 
   end
 
   def compute
 
-    if request.post?
-      list_of_characters = params[:numbers].split(',')
+    list_of_characters = params[:numbers].split(',')
 
-      @list_of_numbers = []
-      @size_of_list = 0
-      @total = 0
+    @list_of_numbers = []
+    @size_of_list = 0
+    @total = 0
 
-      list_of_characters.each do |number|
-        if is_number?(number)
-          @total = @total + number.to_f
-          @size_of_list = @size_of_list + 1
-          @list_of_numbers << number
-        end
+    list_of_characters.each do |number|
+      if is_number?(number)
+        @total = @total + number.to_f
+        @size_of_list = @size_of_list + 1
+        @list_of_numbers << number
       end
+    end
 
-      if @size_of_list < 3
-        @error_message = 'You must have at least 3 numbers.'
-      else
-        @average = @total / @size_of_list
-      end
-
+    if @size_of_list < 3
+      flash[:error] = 'You must have at least 3 numbers.'
+      redirect_to action: index
     else
-
-      redirect_to action: :index
-
+      @average = @total / @size_of_list
     end
 
   end
